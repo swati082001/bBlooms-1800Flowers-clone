587667ec72af07b332b5fbd7dd6afb2eed755601
Suneet25 <suneetpanigrahi2505@gmail.com>
2023-02-21 23:26:07 +0530

    added login page

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,13 +1,9 @@
-
-import './App.css';
-import Cart from './pages/CART/Cart';
-
+import "./App.css";
+// import Cart from './pages/CART/Cart';
+// import AdminSigninPage from "./pages/AdminPage/AdminSignin";
+// import AdminSignupPage from "./pages/AdminPage/AdminSignup";
 function App() {
-  return (
-    <div className="App">
-      <Cart/>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
