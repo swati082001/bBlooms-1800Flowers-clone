a5e406f65bf832933032e3c7524629e362e786e3
swati082001 <swatisucharitamohanty1@gmail.com>
2023-02-22 17:49:02 +0530

    all routes

---
+++ frontend/frontend/src/pages/CHECKOUT/Checkout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Styles from "./Checkout.module.css"
+
+
+const Checkout = () => {
+  return (
+    <div className={Styles.checkout}>Checkout</div>
+  )
+}
+
+export default Checkout
