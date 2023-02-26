655f1cad655f0c607192e4c2413af3d1af5af469
sudipa <sudipahazra.haldia@gmail.com>
2023-02-26 17:20:47 +0530

    cake

--- frontend/frontend/src/index.js
+++ frontend/frontend/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Theme } from "./Theme/Theme";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { store } from "./Redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- frontend/frontend/src/routes/Allroutes.jsx
+++ frontend/frontend/src/routes/Allroutes.jsx
@@ -3,15 +3,16 @@ import Cart from "../pages/CART/Cart"
 import {Routes,Route} from "react-router-dom"
 import Checkout from '../pages/CHECKOUT/Checkout'
 import { Home } from '../pages/Home/Home'
-import Birthday from '../pages/Birthday/ProductPage'
-import SingleProductPage from '../pages/Birthday/SingleProductPage'
 
 import BirthdayCake from '../pages/BirthdayCake/ProductPage'
-import SingleCake from '../pages/BirthdayCake/SingleProductPage'
 import Sympathy from '../pages/Sympathy/ProductPage'
 import { Login } from '../pages/Login/Login'
 import { Register } from '../pages/Register/Register'
 import Main from '../pages/CHECKOUT/Main'
+import Flower from '../pages/Occasion/ProductPage'
+import SingleFlower from '../pages/Occasion/SingleProductPage'
+import SingleCake from '../pages/BirthdayCake/SingleProductPage'
+import SinglePlant from '../pages/Sympathy/SingleProductPage'
 
 
 const Allroutes = () => {
@@ -21,15 +22,19 @@ const Allroutes = () => {
           <Route path='/' element={<Home/>}/>
           <Route path='/login' element={<Login />}/>
           <Route path='/register' element={<Register />}/>
-          <Route path='/birthday' element={<Birthday />}/>
-          <Route path='/birthday/:id' element={<SingleProductPage />}/>
-          <Route path='/sympathy' element={<Sympathy />}/>
-          <Route path='/cart' element={<Cart/>}/>
 
-          <Route path='/checkout' element={<Checkout/>}/>
-          <Route path='/singleproductpage' element={<SingleProductPage/>}/>
+          <Route path='/flower' element={<Flower />}/>
+          <Route path='/flower/:id' element={<SingleFlower/>}/>
+
+        <Route path='/sympathy' element={<Sympathy/>}/>
+        <Route path='/sympathy/:id' element={<SinglePlant/>}/>
           <Route path='/birthdaycake' element={<BirthdayCake/>}/>
-          <Route path='/singlecake' element={<SingleCake/>}/>
+          <Route path='/birthdaycake/:id' element={<SingleCake/>}/>
+          
+
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/checkout' element={<Checkout/>}/>
+          <Route path='/birthdaycake' element={<BirthdayCake/>}/>
           <Route path='/checkout' element={<Main/>}/>
           
 
